66100ec669ef5690a5eb169d7dd844f8205915c9
Diego Esbiza <diegoesbiza@outlook.com>
2022-05-09 19:59:50 -0300

    Modularizando el addtask

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
-//Se crea una IIFE (Inmediately Invoked Fuction Expression) para quitar las funciones creadas del "scope global", es decir, que ninguna de las funciones dentro del IIFE sean accesibles por fuera de ella, limitando asi el acceso al usuario, console.log(s) y a otras funciones que intenten invocarlas externamente
+import { addTask } from "./components/addTask.js";
+
 (() => {
   //abriendo IIFE
   //seleccion del input submit (boton) mediante el atributo "data" en este caso "data-form(de formulario)-btn(boton)" => "data-form-btn".
   const btn = document.querySelector("[data-form-btn]");
-
-  const addTask = (event) => {
-    const list = document.querySelector("[date-list]"); //elemento padre ("ul") que se le va a crear el elemento (mediante appenchild).
-    const task = createTask(event);
-    list.appendChild(task); //agregado del nuevo elemento "li" al elemento padre ("ul").
-
-  }
-
-  const createTask = (event) => {
-    //funcion crear tarea, mediante "arrow function" (funcion anonima).
-    event.preventDefault(); //evita comportamiento por defecto.
-    const input = document.querySelector("[data-form-input]"); //selecciona el "input text" mediante el atributo "data" => data-form-input.
-    const calendar = document.querySelector("[data-form-date]"); //selecciona el "input datetime" en la constante "calendar"
-    const date = calendar.value; //se crea la constante "date" y se le asigna la información de la constante "calendar"
-    const dateFormat = moment(date).format("DD/MM/YY");
-    console.log(date)
-    const value = input.value; //captura del valor (value) del "input text".
-    
-    const task = document.createElement("li"); //creación del elemento "li".
-    task.classList.add("card"); //agregado de clase al lemento "li" creado.
-    input.value = ""; //reiniciar el contenido del input text dejandolo vacio.
-    const taskContent = document.createElement("div"); //creando elemento padre "div".
-    taskContent.appendChild(checkComplete()); //agregado del elemento hijo "checkComplete".
-    const titleTask = document.createElement("span"); //creando el elemnto span que contiene el "value" ingresado.
-    titleTask.classList.add("task"); //agregando clase "task" al span "titleTask" creado.
-    titleTask.innerText = value; //agregandole el "value" ingresado en el input al "task" creado.
-    taskContent.appendChild(titleTask);
-    const dateElement = document.createElement("span"); //creando el elemento que mostrara la fecha en la tarea creada
-    dateElement.innerHTML = dateFormat; //el elemento "dateElement" sera igual al dateformat"
-    task.appendChild(taskContent); //el contenido que tendra el nuevo elemento "li" creado.
-    task.appendChild(dateElement); //agregando la fecha a la tarea creada
-    task.appendChild(deleteIcon()); //agregado icono de borrar.
-    return task;    
-  };
 
   btn.addEventListener("click", addTask); //escuchador de evnto, al hacer click en el boton (btn) se ejecutara la función "createTask".
 })(); //cerrando IIFE
